7a8c1faad5acdf45ddaf8272991a2bd53341f245
kazuki-mura <k.mura0131@gmail.com>
2021-07-17 14:39:46 +0900

    コントローラーの設定

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -4,4 +4,18 @@ class SchedulesController < ApplicationController
     @schedules = Schedule.all
   end
 
+  def new
+    @Schedule = Schedule.new
+  end
+
+  def create
+    Schedule.create(schedule_parameter)
+    redirect_to schedules_path
+  end
+
+  private
+
+  def schedule_parameter
+    params.require(:schedule).permit(:client, :person, :content, :aim, :time, :report, :other, :resource).merge(user_id: current_user.id)
+  end
 end
