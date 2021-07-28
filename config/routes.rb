1a6198c25a2806de8a51873bc3b983a762bb4e4b
kazuki-mura <k.mura0131@gmail.com>
2021-07-28 22:33:55 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'schedules#index'
-  resources :schedules
+  resources :schedules do
+    collection do
+      get 'search'
+    end
+  end
   resources :users, only: :show 
     
 end
