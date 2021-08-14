5ee650bcc3b5e668321b3c0649868bb1b16b48d4
kazuki-mura <k.mura0131@gmail.com>
2021-08-14 17:58:43 +0900

    table headerの表示

--- lib/pdf/practice_pdf/post_pdf.rb
+++ lib/pdf/practice_pdf/post_pdf.rb
@@ -37,16 +37,64 @@ module PracticePdf
     end
     
     def body
-      bounding_box([10, 450], width: 200, heigh:10 ) do
-        @schedules.each {|f| text f.client, size:10}
+      bounding_box([0,450], width: 50, height: 15){
+        stroke_bounds
+        pad(2) {text'訪問日', size: 7, align: :center}
+      }
+      bounding_box([50,450], width: 120, height: 15){
+        stroke_bounds
+        pad(2) {text'会社名', size: 7, align: :center}
+      }
+      bounding_box([170,450], width: 50, height: 15){
+        stroke_bounds
+        pad(2) {text'担当', size: 7, align: :center}
+      }
+      bounding_box([220,450], width: 50, height: 15){
+        stroke_bounds
+        pad(2) {text'商談内容', size: 7, align: :center}
+      }
+      bounding_box([270,450], width: 100, height: 15){
+        stroke_bounds
+        pad(2) {text'目的', size: 7, align: :center}
+      }
+      bounding_box([370,450], width: 60, height: 15){
+        stroke_bounds
+        pad(2) {text'時間', size: 7, align: :center}
+      }
+      bounding_box([430,450], width: 60, height: 15){
+        stroke_bounds
+        pad(2) {text'商品', size: 7, align: :center}
+      }
+      bounding_box([490,450], width: 115, height: 15){
+        stroke_bounds
+        pad(2) {text'報告', size: 7, align: :center}
+      }
+      bounding_box([605,450], width: 115, height: 15){
+        stroke_bounds
+        pad(2) {text'報告', size: 7, align: :center}
+      }
+      bounding_box([720,450], width: 50, height: 15){
+        stroke_bounds
+        pad(2) {text'次回', size: 7, align: :center}
+      }
+
+
+
+      
+      bounding_box([0, 430], width: 50, heigh:10 ) do
+        # @schedules.each {|f| text f.visit_date, size:10} エラーが発生
       end
 
-      bounding_box([200, 450], width: 200, heigh:10) do  
+      bounding_box([50, 430], width: 200, heigh:10 ) do
+        @schedules.each {|f| text f.client, size:7}
+      end
+
+      bounding_box([170, 430], width: 200, heigh:10) do  
        @schedules.each do |f|
         if f.person.present?    
-         text f.person,size:10 
+         text f.person,size:7
         else
-          text " ",size:10 
+          text " ",size:7 
         end
        end
       end
