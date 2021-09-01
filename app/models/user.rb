860a40e298f80deb0d63ada9f6120f8755a1fb14
kazuki-mura <k.mura0131@gmail.com>
2021-09-01 21:07:29 +0900

    fakerの導入と設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+         validates :password, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?[\d])[a-zA-Z\d]+\z/ }
+          with_options presence: true do  
+            with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+              validates :last_name 
+              validates :first_name
+            end
+         end
   has_many :schedules
 
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :user do
-    last_name             {'田中'}
-    first_name            {'太郎'}
-    email                 {'test@example.com'}
-    password              {'000000'}
+    last_name             {Faker::Name.last_name}
+    first_name            {Faker::Name.first_name}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
   end
 end
